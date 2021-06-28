3742af3e50ed81dd9a90861cbe10b18d58e9dafb
deborafsm <deborafreiresoares@outlook.com>
2021-06-28 19:14:22 -0300

    Index de Teoria musical criada

--- starter-template/js/scriptcpf.js
+++ starter-template/js/scriptcpf.js
@@ -1,5 +1,6 @@
 function consultaCep()
 {
+    $(".progress").show();
     var numeroCep = document.getElementById("numeroCep").value;
     console.log(numeroCep);
     var url =   "https://viacep.com.br/ws/"+numeroCep+"/json/";  
@@ -9,14 +10,20 @@ function consultaCep()
         success: function(response){
             console.log(response);
             //console.log(response.bairro); 
-            document.getElementById("cep").innerHTML = response.cep;
+            $("#cep").html("CEP "+response.cep);
             $("#rua").html(response.logradouro);
-            
-            document.getElementById("bairro").innerHTML = response.bairro;
-            document.getElementById("us").innerHTML = response.localidade;
+            $("#bairro").html(response.bairro);
+            $("#us").html(response.localidade);
+            $(".table").show();
+            $(".progress").hide();
         }
     })
 }
+$(function(){
+    $(".table").hide();
+}
+);
 
 //Outra forma de chamar o jquery
 //$("#logradouro").html(response.logradouro);
+//document.getElementById("#").innerHTML = response.localidade;
