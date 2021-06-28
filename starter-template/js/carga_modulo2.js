27b71e9bf81a5bb11ed61e3c69783dd9497dd36c
deborafsm <deborafreiresoares@outlook.com>
2021-06-28 19:28:49 -0300

    Index de Teoria musical criada

---
+++ starter-template/js/carga_modulo2.js
@@ -0,0 +1,37 @@
+let questions = [
+    {
+    numb: 1,
+    question: "As partes que compõe a figura são:",
+    answer: "haste, bandeirola ou colchete e cabeça.",
+    options: [
+      "haste, bandeirola e colchete e cabeça.",
+      "haste, bandeirola ou colchete ou cabeça.",
+      "haste, bandeirola ou colchete e cabeça.",
+      "haste, bandeirola com colchete e cabeça."
+    ]
+  },
+
+    {
+    numb: 2,
+    question: "É correto afirmar que:",
+    answer: "Cada figura tem sua respectiva figura de silêncio.",
+    options: [
+      "Cada figura tem sua respectiva figura de silêncio.",
+      "Cada figura não tem sua respectiva figura de silêncio.",
+      "Cada figura não possui figura de silêncio.",
+      "Cada figura tem segue de respectiva figura de silêncio."
+    ]
+  },
+    {
+    numb: 3,
+    question: "De acordo com as bandeirolas é correto afirmar que:",
+    answer: "As colcheias, semicolcheias, fusas e semifusas podem ser escritas com bandeirolas ou ligadas por barra de união",
+    options: [
+      "As colcheias, semicolcheias, fusas e semifusas podem ser escritas com bandeirolas ou ligadas por barra de união",
+      "As colcheias, semicolcheias, fusas ou semifusas podem ser escritas com bandeirolas ou ligadas por barra de união",
+      "As colcheias, semicolcheias, fusas e semifusas podem ser escritas com bandeirolas e ligadas por barra de união",
+      "As colcheias, semicolcheias, fusas somente semifusas podem ser escritas com bandeirolas ou ligadas por barra de união"
+    ]
+  },
+    
+];
